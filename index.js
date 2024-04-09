bc67fda044ba97bb747f542570b240f11f0771d6
Goyal-Puja <goyalpuja48@gmail.com>
2024-04-09 20:11:41 +0530

    Task-1

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const connectToMongo = require("./db");
 const express = require("express");
+const axios = require("axios");
+const Crypto = require("./CryptoModel");
+const cron = require("node-cron");
 
 connectToMongo();
 
 const app = express();
 const port = 5000;
 
+async function updateCrypto() {
+  try {
+    const response = await axios(
+      "https://api.coingecko.com/api/v3/coins/list?include_platform=false"
+    );
+    const cryptos = response.data;
+    await Crypto.deleteMany({});
+    await Crypto.insertMany(cryptos);
+    console.log("Cryptocurrency list updated successfully!");
+  } catch (error) {
+    console.error("Error updating cryptocurrency list:", error.message);
+  }
+}
+
+cron.schedule("0 * * * *", () => {
+  console.log("Running updateCrypto Job..");
+  updateCrypto();
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
+
+updateCrypto();
